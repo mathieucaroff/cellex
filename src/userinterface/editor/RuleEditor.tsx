4cb314916819d06f0257704ee08f9a51c495828a
Mathieu CAROFF <mathieu.caroff@free.fr>
2022-04-12 15:28:36 +0200

    add upgrade state count button to rule editor

--- src/userinterface/editor/RuleEditor.tsx
+++ src/userinterface/editor/RuleEditor.tsx
@@ -140,6 +140,34 @@ export let RuleEditor = () => {
                     Simplify
                 </Button>
                 <Button
+                    disabled={rule.stateCount >= 6}
+                    onClick={() => {
+                        context.updateState(({ rule }) => {
+                            let oldLength = rule.transitionFunction.length
+                            let osc = rule.stateCount // old state count
+
+                            rule.stateCount += 1
+
+                            let sc = rule.stateCount
+                            let length = sc ** 3
+                            let tf = Array.from({ length }, (_, k) => {
+                                let n = length - 1 - k
+                                let left = Math.floor(n / sc ** 2)
+                                let middle = Math.floor(n / sc) % sc
+                                let right = n % sc
+                                if (left + 1 >= sc || middle + 1 >= sc || right + 1 >= sc) {
+                                    return middle
+                                }
+                                let index = left * osc ** 2 + middle * osc + right
+                                return rule.transitionFunction[oldLength - 1 - index]
+                            })
+                            rule.transitionFunction = tf
+                        })
+                    }}
+                >
+                    Upgrade state count to {rule.stateCount + 1} ({"__btqph_"[rule.stateCount + 1]})
+                </Button>
+                <Button
                     disabled={deepEqual(rule.transitionFunction, complement.transitionFunction)}
                     onClick={() => {
                         context.updateState(({ rule }) => {
