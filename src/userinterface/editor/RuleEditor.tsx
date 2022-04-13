2fb1cd0cece3ef3f6e94c97a3871ef036f6a5ba9
Mathieu CAROFF <mathieu.caroff@free.fr>
2022-04-13 04:49:38 +0200

    improve the rule upgrade algorithm

--- src/userinterface/editor/RuleEditor.tsx
+++ src/userinterface/editor/RuleEditor.tsx
@@ -140,9 +140,11 @@ export let RuleEditor = () => {
                                 let left = Math.floor(n / sc ** 2)
                                 let middle = Math.floor(n / sc) % sc
                                 let right = n % sc
-                                if (left + 1 >= sc || middle + 1 >= sc || right + 1 >= sc) {
-                                    return middle
-                                }
+                                // if any of left, middle or right too big for the old rule,
+                                // we should default to the highest acceptable value
+                                left = Math.min(left, osc - 1)
+                                middle = Math.min(middle, osc - 1)
+                                right = Math.min(right, osc - 1)
                                 let index = left * osc ** 2 + middle * osc + right
                                 return rule.transitionFunction[oldLength - 1 - index]
                             })
