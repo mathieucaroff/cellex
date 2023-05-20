86ffe0e9fef8cd47a176627dd217c437646b5863
Mathieu CAROFF <mathieu.caroff@free.fr>
2023-05-20 13:39:56 +0200

    disable react integration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import react from "@vitejs/plugin-react"
+// import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
 
 import nearley from "./vite-plugin-nearley"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [nearley(), react()],
+  plugins: [nearley()],
 })
