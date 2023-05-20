3e53bee80e45e14fa6bf529b566cb6fbc9ad42d3
Mathieu CAROFF <mcaroff@wallix.com>
2023-05-20 21:14:22 +0200

    add links to WolframAlpha, Mathworld and Wikipedia

--- src/userinterface/RuleInfo.tsx
+++ src/userinterface/RuleInfo.tsx
@@ -4,19 +4,19 @@ import { Rule } from "../ruleType"
 import { useStateSelection } from "./hooks"
 
 export const informationSet: Record<string, string> = {
-  b30: "is famous for its use in artistic projects and as a source of chaos to generate randomness in some Wolfram products",
-  b54: "is of Class 4. It is thought to be Turing Complete, though this remains to be proven.",
-  b60: "ignores the value to the right of the cell and XOR-s the value of the cell with the one to its left. It is like it computes a tilted cellular automaton with a neighborhood size of two",
-  b90: "ignores the current value of the cell and set it to the result of XOR-ing the cell to the left and the cell to the right. Because it ignores its own value, it is like there are two distinct cellular automaton with a neighborhood size of two being computed in parallel.",
-  b105: "computes the negation of the result XOR-ing all three cells. Rule 105 is like Rule 150, but twinkling.",
-  b106: "is of Class 4. It is thought to be Turing Complete, though this remains to be proven.",
-  b110: "is of Class 4 and it proven to be Turing Complete.",
-  b150: "computes the result of XOR-ing all three cells.",
-  b184: "is famous for being a simple simulation of objects moving to the right whenever there is room for them.",
+  e30: "is famous for its use in artistic projects and as a source of chaos to generate randomness in some Wolfram products.",
+  e54: "is of Class 4. It is thought to be Turing Complete, though this remains to be proven.",
+  e60: "ignores the value to the right of the cell and XOR-s the value of the cell with the one to its left. It is like it computes a tilted cellular automaton with a neighborhood size of two.",
+  e90: "ignores the current value of the cell and set it to the result of XOR-ing the cell to the left and the cell to the right. Because it ignores its own value, it is like there are two distinct cellular automaton with a neighborhood size of two being computed in parallel.",
+  e105: "computes the negation of the result of XOR-ing all three cells. Rule 105 is like Rule 150, but twinkling.",
+  e106: "is of Class 4. It is thought to be Turing Complete, though this remains to be proven.",
+  e110: "is of Class 4 and it proven to be Turing Complete.",
+  e150: "computes the result of XOR-ing all three cells.",
+  e184: "is famous for being a simple simulation of objects moving to the right whenever there is room for them.",
   // primary rules
-  b51: "is the negation rule. It swaps the state of each cell between alive and dead at each generation.",
-  b170: "is the shift-left rule. The state of each cell is shifted by one at each generation.",
-  b204: "is the identity rule. It leaves the world unchanged.",
+  e51: "is the negation rule. It swaps the state of each cell between alive and dead at each generation.",
+  e170: "is the shift-left rule. The state of each cell is shifted by one at each generation.",
+  e204: "is the identity rule. It leaves the world unchanged.",
 }
 
 interface Info {
@@ -66,9 +66,11 @@ export let RuleInfo = () => {
     )
   } else {
     return (
-      <span>
-        Rule {name} is a symmetric of rule {info.rule} which {info.extra}
-      </span>
+      <>
+        <span>
+          Rule {name} is a symmetric of rule {info.rule} which {info.extra}
+        </span>
+      </>
     )
   }
 }
---
+++ src/userinterface/editor/link.ts
@@ -0,0 +1,61 @@
+import { computeTransitionNumber } from "../../engine/rule"
+import { Rule } from "../../ruleType"
+
+export let getMathworldLink = (ruleNumber: number) => {
+  let knownRuleNumbers = [
+    30, 50, 54, 60, 62, 90, 94, 102, 110, 126, 150, 158, 182, 188, 190, 220, 222,
+  ]
+  if (knownRuleNumbers.includes(ruleNumber)) {
+    return `https://mathworld.wolfram.com/Rule${ruleNumber}.html`
+  }
+  return ""
+}
+
+export let getWikipediaLink = (ruleNumber: number) => {
+  let knownRuleNumbers = [28, 50, 54, 60, 90, 94, 102, 110, 150, 158, 188, 190, 220, 222]
+  if (knownRuleNumbers.includes(ruleNumber)) {
+    return `https://en.wikipedia.org/wiki/Elementary_cellular_automaton#Rule_${ruleNumber}`
+  }
+  return ""
+}
+
+export let getWikipediaDedicatedPageLink = (ruleNumber: number) => {
+  let knownRuleNumbers = [90, 110, 184]
+  if (knownRuleNumbers.includes(ruleNumber)) {
+    return `https://en.wikipedia.org/wiki/Rule_${ruleNumber}`
+  }
+  return ""
+}
+
+export let getWolframAlphaLink = (rule: Rule) => {
+  let prefix = "https://www.wolframalpha.com/input/?i="
+  let r = fractionToString(rule.neighborhoodSize - 1, 2)
+  let ruleNumber = computeTransitionNumber(rule)
+  return `${prefix}k%3D${rule.stateCount}+r%3D${r}+rule+${ruleNumber}`
+}
+
+let fractionToString = (numerator: number, denominator: number) => {
+  let g = gcd(numerator, denominator)
+  let result = String(numerator / g)
+  if (denominator > g) {
+    result += `/${denominator / g}`
+  }
+
+  return result
+}
+
+let gcd = (a: number, b: number) => {
+  a = Math.abs(a)
+  b = Math.abs(b)
+  if (b > a) {
+    var temp = a
+    a = b
+    b = temp
+  }
+  while (true) {
+    if (b == 0) return a
+    a %= b
+    if (a == 0) return b
+    b %= a
+  }
+}
